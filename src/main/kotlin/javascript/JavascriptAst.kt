e40473037097d7939773be6bad8c64d36dc3c7e6
Raas Ahsan <agro@jantox.com>
2018-12-24 23:01:37 -0600

    start defining javascript ast

--- src/main/kotlin/javascript/JavascriptAst.kt
+++ src/main/kotlin/javascript/JavascriptAst.kt
@@ -1,3 +1,23 @@
 package javascript
 
 sealed class JavascriptAst
+
+data class VarDeclaration(val name: String) // Can declare multiple variables and assign at the same time
+
+data class VarAssignment(val identifier: String, val rhs: Int)
+
+data class FunctionDefinition(val name: String, val arguments: List<String>, val body: Int)
+
+data class FunctionInvocation(val name: String, val arguments: List<Int>)
+
+data class Return(val expression: Int)
+
+data class Add(val n: Int)
+
+data class Multiply(val n: Int)
+
+data class Subtract(val n: Int)
+
+data class Divide(val n: Int)
+
+// TODO: If-else and if-else if-else expressions
